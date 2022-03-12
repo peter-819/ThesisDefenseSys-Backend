38fd5cbaf42180525e97b74d9ee5932171a6cad2
peter-819 <peter-819@outlook.com>
2022-03-12 21:14:35 +0800

    Initial commit

---
+++ TDS/internal/svc/servicecontext.go
@@ -0,0 +1,15 @@
+package svc
+
+import (
+	"TDS-backend/TDS/internal/config"
+)
+
+type ServiceContext struct {
+	Config config.Config
+}
+
+func NewServiceContext(c config.Config) *ServiceContext {
+	return &ServiceContext{
+		Config: c,
+	}
+}
